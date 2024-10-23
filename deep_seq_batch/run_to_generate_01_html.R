b9e05eb5d0856ce7315f37fa1418f4d1a100da10
trnguyenuka <trnguyenuka@github.com>
2024-10-23 13:53:19 +0200

    update

--- deep_seq_batch/run_to_generate_01_html.R
+++ deep_seq_batch/run_to_generate_01_html.R
@@ -9,7 +9,8 @@ path.to.project.src <- "/home/hieunguyen/CRC1382/src_2023/SBharadwaj/deep_seq_ba
 source(file.path(path.to.project.src, "00_import_libraries.R"))
 source(file.path(path.to.project.src, "00_helper_functions.R"))
 
-outdir <- "/home/hieunguyen/CRC1382/outdir"
+# outdir <- "/home/hieunguyen/CRC1382/outdir"
+outdir <- "/media/hieunguyen/HD01/outdir/CRC1382/SBharadwaj_20240318"
 
 path.to.main.src <- "/home/hieunguyen/CRC1382/src_2023/SBharadwaj/deep_seq_batch"
 
@@ -23,10 +24,10 @@ path.to.main.src <- "/home/hieunguyen/CRC1382/src_2023/SBharadwaj/deep_seq_batch
 #                   "SBharadwaj_20240318_Sample_2_3_5_6",
 #                   "SBharadwaj_20240318_Sample_1_4_7_8_2_5")
 
-all.PROJECTS <- c("SBharadwaj_20240318_Sample_3_6")
+all.PROJECTS <- c("SBharadwaj_20240318_Sample_1_4_7_8")
 
 for (PROJECT in all.PROJECTS){
-  path.to.html.outputs <- file.path(outdir, "SeuratV5", PROJECT, "html_output")
+  path.to.html.outputs <- file.path(outdir, PROJECT, "html_output")
   dir.create(path.to.html.outputs, showWarnings = FALSE, recursive = TRUE)
   
   path.to.Rmd.file <- file.path(path.to.main.src, "01_preliminary_analysis_all_samples_integrated.Rmd")
